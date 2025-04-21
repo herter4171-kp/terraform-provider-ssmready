d1d24c7b97726c86d817134d4c649b13052638fc
herter4171-kp <herter@kairospower.com>
2025-04-21 18:31:28 -0400

    Print to stdout.

--- provider/resource_instance_ready.go
+++ provider/resource_instance_ready.go
@@ -1,10 +1,9 @@
-
 package provider
 
 import (
     "context"
     "fmt"
-    "log"
+    "os"
     "time"
 
     "github.com/aws/aws-sdk-go/aws"
@@ -12,6 +11,12 @@ import (
     "github.com/hashicorp/terraform-plugin-sdk/v2/diag"
     "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
+
+const ssmreadyPrefix = "\033[36m[ssmready]\033[0m" // cyan
+
+func printStatus(format string, a ...interface{}) {
+    fmt.Fprintf(os.Stderr, "%s %s\n", ssmreadyPrefix, fmt.Sprintf(format, a...))
+}
 
 func resourceInstanceReady() *schema.Resource {
     return &schema.Resource{
@@ -53,7 +58,7 @@ func resourceInstanceReadyCreate(ctx context.Context, d *schema.ResourceData, me
     timeout := d.Get("timeout").(int)
     interval := d.Get("interval").(int)
 
-    log.Printf("[INFO] Waiting up to %d seconds for instances to become available in SSM", timeout)
+    printStatus("Waiting up to %d seconds for instances to become available in SSM", timeout)
 
     deadline := time.Now().Add(time.Duration(timeout) * time.Second)
 
@@ -92,7 +97,7 @@ func resourceInstanceReadyCreate(ctx context.Context, d *schema.ResourceData, me
         }
 
         if allReady {
-            log.Printf("[INFO] All instances ping as online.  Waiting for Fleet Manager")
+            printStatus("All instances ping as online. Waiting for Fleet Manager")
             break
         }
 
@@ -103,7 +108,6 @@ func resourceInstanceReadyCreate(ctx context.Context, d *schema.ResourceData, me
         waitForInventoryPresence(client, *id, 10*time.Minute)
     }
 
-    // Just use a deterministic ID from the list
     d.SetId(fmt.Sprintf("ssm-ready-%d", time.Now().Unix()))
     return nil
 }
@@ -112,12 +116,10 @@ func waitForInventoryPresence(ssmClient *ssm.SSM, instanceID string, timeout tim
     deadline := time.Now().Add(timeout)
 
     for {
-        // Check for timeout
         if time.Now().After(deadline) {
             return fmt.Errorf("Timeout waiting for instance %s to appear in SSM inventory", instanceID)
         }
 
-        // Call GetInventory with filter on the specific instance ID
         resp, err := ssmClient.GetInventory(&ssm.GetInventoryInput{
             Filters: []*ssm.InventoryFilter{
                 {
@@ -132,9 +134,8 @@ func waitForInventoryPresence(ssmClient *ssm.SSM, instanceID string, timeout tim
             return fmt.Errorf("Error querying inventory for instance %s: %w", instanceID, err)
         }
 
-        // If inventory data is returned, assume instance is Fleet Manager ready
         if len(resp.Entities) > 0 {
-            log.Printf("[INFO] Instance %s is in Fleet Manager", instanceID)
+            printStatus("Instance %s is in Fleet Manager", instanceID)
             return nil
         }
 
